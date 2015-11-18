a0643a3ec3dfc4631c7ad906f9c2948905ebdff8
Ubuntu <ubuntu@ip-10-0-0-31.us-west-2.compute.internal>
2015-11-18 22:51:12 +0000

    while returning the shortUrl, it will also return the host's public IP

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var fs = require('fs');
 var express = require('express');
 var aws = require('aws-sdk');
 var sh = require('shorthash');
+var request = require('request');
 
 var app = express();
 app.use(express.bodyParser());
 
 var queueUrl = "https://sqs.us-west-2.amazonaws.com/060340690398/team6cp";
+var ipUrl = "http://169.254.169.254/latest/meta-data/public-ipv4";
 
 // Load your AWS credentials and try to instantiate the object.
 aws.config.loadFromPath(__dirname + '/config.json');
@@ -17,8 +19,18 @@ var sqs = new aws.SQS();
 var convertURL = function(longurl, callback) {
     //shorturl = crypto.createHash('md5').update(longurl).digest("hex");
     shorturl = sh.unique(longurl)
-    //sendMessageSQS(longurl, shorturl);
+    sendMessageSQS(longurl, shorturl);
     callback(shorturl);
+}
+
+var getPublicIP = function(callback){
+    request({
+        url : ipUrl,
+        method : "GET"
+    }, function(error, response, body){
+        console.log("getting IP: " + body);
+        callback(body);
+    })
 }
 
 var sendMessageSQS = function(longurl, shorturl){
@@ -46,13 +58,15 @@ var sendMessageSQS = function(longurl, shorturl){
 var handle_post = function (req, res) {
     console.log("Post: ..." );
     console.log(req.body.longurl);
+    getPublicIP( function(publicIP){
     longurl = req.body.longurl;
     convertURL(longurl, function(shorturl){
         console.log(shorturl);
         res.setHeader('Content-Type', 'application/json');
-        var data = {"shorturl": shorturl};
+        var data = {shorturl: shorturl, publicIP:publicIP};
         console.log(data);
         res.json(data);
+    })
     });
 }
 app.post("*", handle_post );
